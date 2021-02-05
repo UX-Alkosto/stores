e3ea698142ee6a73c783caed3b2ba6aa9ac0ed7a
alejaCrea20 <61421003+alejaCrea20@users.noreply.github.com>
2021-02-05 17:24:11 -0500

    Actualización css
    Modificación estilos.

--- dist/common/js/ciudades.js
+++ dist/common/js/ciudades.js
@@ -1,2 +1,2 @@
-/*! stores - v1.2.9 */
+/*! stores - v1.3.0 */
 import{stores}from"./stores/index.js";(()=>{if(storesJsonFile){const e=stores.getStoreFromUrl();stores.log(e),stores.get(storesJsonFile).then(({ciudades:r})=>{const o=r[e.city],t=r[e.city][0],s=document.querySelector("#stores"),n=[];Object.keys(r[e.city].tiendas).length&&(document.querySelectorAll(".nombre-ciudad").length&&(document.querySelector(".nombre-ciudad").innerHTML=o.label),Object.values(o.tiendas).forEach(e=>{n.push(stores.render.store({address:e.direccion,city:o.label,howToGet:e.como_llegar,label:t,link:e.ir,name:e.nombre_tienda,schedule:e.horario_apertura}))}),Promise.all(n).then(e=>{s.innerHTML="",e.forEach(e=>s.insertAdjacentHTML("beforeend",`${e}`))}))})}})();
