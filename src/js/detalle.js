b0b7c677875657f2bc1d88307a6dd8e9a853e065
Miguel Colmenares <me@miguelcolmenares.com>
2021-02-21 16:45:27 -0500

    💥 Refactor JS
    Created Stores Class
    Created Store Class
    ➕ Added Babel
    ➕ Added Eslint

--- src/js/detalle.js
+++ src/js/detalle.js
@@ -1,49 +1,56 @@
-import { stores } from "./stores/index.js"
-(() => {
-	if (storesJsonFile) {
-		const params = stores.getStoreFromUrl()
-		stores.log(params)
-		stores.get(storesJsonFile).then(async ({ ciudades }) => {
-			const store = ciudades[params.city].tiendas[params.store],
-				storesContainer = document.querySelector("#stores"),
-				storesDetails = document.querySelector(".contenedor_tienda_detalle"),
-				storesPromises = [],
-				cityLabel = document.querySelectorAll('.nombre-ciudad')
+import { Stores, Store, log } from "./stores/index.js";
+(async (document, storesJsonFile) => {
+	try {
+		const Tiendas = new Stores(storesJsonFile),
+			params = Stores.getStoreFromUrl(),
+			storesContainer = document.querySelector("#stores"),
+			storeDetails = document.querySelector(".contenedor_tienda_detalle"),
+			cityLabel = document.querySelectorAll(".nombre-ciudad"),
+			otherStores = document.querySelector(".tit_otras");
 
-			storesContainer.innerHTML = ""
-			cityLabel.forEach(label => label.innerHTML = ciudades[params.city].label)
-			storesDetails.insertAdjacentHTML('beforeend', await stores.render.detail({
+		log(params);
+
+		await Tiendas.stores.then(async ({ ciudades }) => {
+			const store = ciudades[params.city].tiendas[params.store];
+
+			storesContainer.innerHTML = "";
+			cityLabel.forEach(label => label.innerHTML = ciudades[params.city].label);
+			const Tienda = new Store({
 				address: store.direccion,
 				howToGet: store.como_llegar,
-				image: store.imagen,
 				link: store.ir,
+				external: store.externo,
 				map: store.ver_mapa,
 				name: store.nombre_tienda,
 				schedule: store.horario_apertura,
 				scheduleDetail: store.horarios
-			}))
+			});
+
+			storeDetails.append(document.createRange().createContextualFragment(await Tienda.render("detail")));
+
 			if (Object.keys(ciudades[params.city].tiendas).length > 1) {
-				Object.entries(ciudades[params.city].tiendas).forEach(storeData => {
-					const label = storeData[0];
-					const store = storeData[1];
-					if (label == params.store) return
-					storesPromises.push(stores.render.store({
+				// eslint-disable-next-line no-undef
+				Object.entries(ciudades[params.city].tiendas).map(async ([label, store] = tiendas) => {
+					if (label == params.store) return;
+
+					const Tienda = new Store({
 						address: store.direccion,
 						city: ciudades[params.city].label,
 						howToGet: store.como_llegar,
-						label: params.city,
+						label: label,
 						link: store.ir,
+						external: store.externo,
 						name: store.nombre_tienda,
 						schedule: store.horario_apertura
-					}))
-				})
-				Promise.all(storesPromises).then(storesHtml => {
-					storesContainer.innerHTML = ""
-					storesHtml.forEach(storeHtml => storesContainer.insertAdjacentHTML('beforeend', `${storeHtml}`))
-				})
+					});
+					storesContainer.append(document.createRange().createContextualFragment(await Tienda.render()));
+				});
 			} else {
-				document.querySelector('.tit_otras').remove()
+				otherStores.remove();
 			}
-		})
+		});
+	} catch (error) {
+		log(error, "error");
 	}
-})();
+	// eslint-disable-next-line no-undef
+})(document, storesJsonFile);
--- src/js/stores/index.js
+++ src/js/stores/index.js
@@ -1,118 +1,170 @@
-export const stores = {
-	get: async (jsonUrl = '') => {
-		if (jsonUrl.length) {
-			return await fetch(jsonUrl, {
-				cache: 'force-cache',
-				mode: 'cors'
-			})
-				.then(response => response.json())
-				.then(data => data)
+function log(message, type = "") {
+	let color = "";
+	switch (type) {
+		case "error":
+			color = "#dd171b";
+			break;
+		case "info":
+			color = "#2450f9";
+			break;
+		case "warning":
+			color = "#e4a003";
+			break;
+		default:
+			color = "#2ecc71";
+			break;
+	}
+	const logStyles = `background: ${color}; border-radius: 0.5em;color: white; font-weight: bold; padding: 2px 0.5em`;
+	return console.log("%cStores", logStyles, message);
+}
+
+class Stores {
+	constructor(json = undefined) {
+		this.json = json;
+	}
+
+	get stores() {
+		return this.getStores();
+	}
+
+	async getStores() {
+		if (this.json.length) {
+			return await fetch(this.json, {
+				cache: "force-cache",
+				mode: "cors"
+			}).then(response => {
+				if (response.ok) return response.json();
+				else throw `Unable to load Json file ${this.json}`;
+			}).then(data => data);
+		} else {
+			throw `Unable to load Json file ${this.json}`;
 		}
-	},
-	getStoreFromUrl: (location = window.location) => {
-		const url = new URL(location);
-		let city, store, urlParams = url.pathname.split('/');
+	}
+
+	static getStoreFromUrl() {
+		const url = new URL(window.location);
+		let city, store, urlParams = url.pathname.split("/");
 		urlParams.map((value, key) => {
 			if (value.length) {
-				if (value === 'tiendas') {
-					city = urlParams[key + 1]
-					store = urlParams[key + 2]
-					if(store === 'c')
-						store = urlParams[key + 3]
+				if (value === "tiendas") {
+					city = urlParams[key + 1];
+					store = urlParams[key + 2];
+					if (store === "c")
+						store = urlParams[key + 3];
 				}
 			}
-		})
-		return { city, store }
-	},
-	isOpen: async (schedule = []) => {
-		if (schedule.length) {
-			const module = await import('./isopen.js')
-			return module.isOpen(schedule)
-		}
-	},
-	log: message => {
-		const logStyles = 'background: #2ecc71; border-radius: 0.5em;color: white; font-weight: bold; padding: 2px 0.5em'
-		return console.log('%cStores', logStyles, message)
-	},
-	render: {
-		detail : async ({
-			address = '',
-			howToGet = '',
-			image = '',
-			map = '',
-			name = '',
-			schedule = [],
-			scheduleDetail = []
-		}) => {
-			const isOpen = await stores.isOpen(schedule)
-			return `
+		});
+		return {
+			city,
+			store
+		};
+	}
+}
+
+class Store {
+	constructor({
+		address = "",
+		city = "",
+		howToGet = "",
+		image = "",
+		label = "",
+		link = "",
+		external = false,
+		map = "",
+		name = "",
+		order = 0,
+		schedule = [],
+		scheduleDetail = []
+	}) {
+		this.address = address;
+		this.city = city;
+		this.external = external;
+		this.howToGet = howToGet;
+		this.image = image;
+		this.label = `todas ${label}`;
+		this.link = link;
+		this.map = map;
+		this.name = name;
+		this.order = order;
+		this.schedule = schedule;
+		this.scheduleDetail = scheduleDetail;
+	}
+
+	get isOpen() {
+		return this._isOpen();
+	}
+
+	async render(type = "store") {
+		const isOpen = await this.isOpen;
+		switch (type) {
+			case "detail":
+				return `
 			<div class="bg-tienda"></div>
 			<div class="imagen_tienda">
-				<img src="${image}" alt="${name}">
+				<img src="${this.image}" alt="${this.name}">
 			</div>
 			<div class="detalle_tienda">
 				<div class="info_detalle">
-					<h2>${name}</h2>
-					<span class="direc">${address}</span>
-					<p class="apertura ${isOpen.open ? 'verde' : 'rojo'}">${isOpen.message}</p>
+					<h2>${this.name}</h2>
+					<span class="direc">${this.address}</span>
+					<p class="apertura ${isOpen.open ? "verde" : "rojo"}">${isOpen.message}</p>
 					<span class="tit_horario">
 						<i class="alk-icon-clock"></i>
 						Horarios
 					</span>
 					<div class="horarios_detalle">
-						${scheduleDetail.map(detail => `<p>${detail}</p>`).join('')}
+						${this.scheduleDetail.map(detail => `<p>${detail}</p>`).join("")}
 					</div>
 					<div class="links_detalle">
-						${howToGet.length ? `<span><a href="${howToGet}" target="_blank" rel="noopener" title="Como llegar">
+						${this.howToGet.length ? `<span><a href="${this.howToGet}" target="_blank" rel="noopener" title="Como llegar">
 							<i class="alk-icon-arrive"></i>
 							Como llegar
-						</a></span>` : ''}
-						${map.length ? `<span><a href="${map}" target="_blank" rel="noopener" title="Ver mapa">
+						</a></span>` : ""}
+						${this.map.length ? `<span><a href="${this.map}" target="_blank" rel="noopener" title="Ver mapa">
 							<i class="alk-icon-mapa"></i>
 							Ver mapa
-						</a></span>` : ''}
+						</a></span>` : ""}
 					</div>
 				</div>
 			</div>`;
-		},
-		store: async ({
-			address = '',
-			city = '',
-			howToGet = '',
-			label = '',
-			link = '',
-			external = false,
-			name = '',
-			schedule = []
-		}) => {
-			const isOpen = await stores.isOpen(schedule)
-			return `<div class="store" data-city="${label}">
-				<h2 class="ciudad_mun">${city}</h2>
+			default:
+				return `<div style="order: ${this.order}" class="store" data-city="${this.label}">
+				<h2 class="ciudad_mun">${this.city}</h2>
 				<div class="recuadroUno">
 					<h3 class="nombre">
 						<i class="alk-icon-nuestras-tiendas"></i>
-						${name}
+						${this.name}
 					</h3>
 					<div class="ir">
-						<a href="${link.length ? `${link}` : '#'}" target="${external ? '_blank' : '_self'}" rel="noopener" title="${name} - ${city}">
+						<a href="${this.link.length ? `${this.link}` : "#"}" target="${this.external ? "_blank" : "_self"}" rel="noopener" title="${this.name} - ${this.city}">
 							<i class="alk-icon-arrow-right-square"></i>
 						</a>
 					</div>
 				</div>
-				<p class="direccionTienda">${address}</p>
-				<p class="apertura ${isOpen.open ? 'verde' : 'rojo'}">${isOpen.message}</p>
+				<p class="direccionTienda">${this.address}</p>
+				<p class="apertura ${isOpen.open ? "verde" : "rojo"}">${isOpen.message}</p>
 				<div class="indicaciones">
-					${link.length ? `<div class="ver_horario">
+					${this.link.length ? `<div class="ver_horario">
 							<i class="alk-icon-mapa"></i>
-							<a href="${link}" target="${external ? '_blank' : '_self'}" rel="noopener" title="Ver mapa y horarios">Ver mapa y horarios</a>
-						</div>` : ''}
-					${link.length ? `<!--<div class="comoLlegar">
+							<a href="${this.link}" target="${this.external ? "_blank" : "_self"}" rel="noopener" title="Ver mapa y horarios">Ver mapa y horarios</a>
+						</div>` : ""}
+					${this.link.length ? `<!--<div class="comoLlegar">
 							<i class="alk-icon-mapa"></i>
-							<a href="${howToGet}" target="_blank" rel="noopener" title="Cómo llegar">Cómo llegar</a>
-						</div>-->` : ''}
+							<a href="${this.howToGet}" target="_blank" rel="noopener" title="Cómo llegar">Cómo llegar</a>
+						</div>-->` : ""}
 				</div>
-				<a class="click" href="${link.length ? `${link}` : '#'}" target="${external ? '_blank' : '_self'}" rel="noopener" title="${name} - ${city}"></a>
-			</div>`
+				<a class="click" href="${this.link.length ? `${this.link}` : "#"}" target="${this.external ? "_blank" : "_self"}" rel="noopener" title="${this.name} - ${this.city}"></a>
+			</div>`;
 		}
 	}
+
+	async _isOpen() {
+		return (await import("./isopen.js")).isOpen(this.schedule);
+	}
 }
+
+export {
+	Stores,
+	Store,
+	log
+};
